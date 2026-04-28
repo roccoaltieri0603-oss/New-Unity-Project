b63f64be4c52d09627aa2c6cca2dda084064379d
roccoaltieri0603-oss <roccoaltieri0603@gmail.com>
2026-04-28 14:22:35 -0300

    Initial commit

---
+++ Assets/Scrips/PincheScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PincheScript : MonoBehaviour
+{
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+        rotationSpeed = 3f;
+        }
+        if(Input.GetKeyUp(KeyCode.Space)){
+            rotationSpeed = 0f;
+        }
+        transform.Rotate(0,rotationSpeed,0);
+    }
+}
